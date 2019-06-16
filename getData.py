a4f25d8e7a0e7acea429c5478da9236e862b6521
rheakk <rheakk3@gmail.com>
2019-06-16 07:53:38 -0400

    Split getData into electionData and getData modules.

--- getData.py
+++ getData.py
@@ -1,27 +1,53 @@
+"""
+    Author: Rhea Kothari
+    Created: 16-June-2019
+
+    A common module that allows for downloading of files from url.
+
+    Also will will return as Pandas Data Frame and save data to sql database
+"""
 import os.path
 import logging
 from urllib import request
 import pandas as pd
 
 
-def fetchData (url, out_path, sep=','):
-    ''' get data from url which is used to to be a link downloadable data
+def saveUrl (url, outPath, sep=','):
+    '''
+        get data from url which is a link to downloadable data file
+
+        outPath - is the path of the filename where data needs to be saved
+
+        returns outPath of the file downloaded.
     '''
 
     # if the file already exists then do not download. and use existing downloaded
     # file
-    if os.path.isfile(out_path):
-        logging.info("Filename %s already downloaded", out_path)
+    if os.path.isfile(outPath):
+        logging.info("Filename %s already downloaded", outPath)
     else:
         logging.info("downloading using %s", url)
         try:
-            request.urlretrieve(url, out_path)
-        except:
-            logging.exception ("Could not download url:%s into %s", url, out_path)
-            exit(1)
-        logging.info("downloaded %s", out_path)
+            request.urlretrieve(url, outPath)
+        except Exception as ex:
+            logging.exception ("Could not download url:%s into %s", url, outPath)
+            raise ex
 
-    data = pd.read_csv(out_path, sep=sep)
+    logging.info("downloaded %s", outPath)
+    return outPath
+
+def getUrlPd (url, outPath, sep=','):
+    '''
+        get data from url which is a link downloadable data file and returns
+        as a pandas data frame
+
+        sep in the file for parsing - e.g. '\t' for a tab delimited file
+    '''
+
+    urlFile = saveUrl (url, outPath)
+
+    data = pd.read_csv(urlFile, sep=sep)
+
     logging.debug("Loaded data:\n%r", data.head())
 
     return data
